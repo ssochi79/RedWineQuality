631cfb46c648d8acf7e14631580f98f74c49c7e3
ssochi79 <112685588+ssochi79@users.noreply.github.com>
2025-06-04 19:14:10 +0900

    Add files via upload

---
+++ train_model.py
@@ -0,0 +1,10 @@
+from sklearn.datasets import load_iris
+from sklearn.ensemble import RandomForestClassifier
+import joblib
+
+X, y = load_iris(return_X_y=True)
+model = RandomForestClassifier()
+model.fit(X, y)
+
+joblib.dump(model, 'model.pkl')
+print("model.pkl 저장 완료")
